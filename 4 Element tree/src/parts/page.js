b5f061b4ef6f36e347960837b610eb45a19b7843
Marcus Downing <marcus@bang-on.net>
2018-11-30 19:33:09 +0000

    Money field spacing

--- 4 Element tree/src/parts/page.js
+++ 4 Element tree/src/parts/page.js
@@ -1,4 +1,4 @@
-const copyrightAttribution = `<div class='copyright-attribution'><p>
+const paizoCopyrightAttribution = `<div class='copyright-attribution'><p>
 <b>&copy; Marcus Downing &nbsp; <a href='https://www.dyslexic-charactersheets.com/'>dyslexic-charactersheets.com</a></b>
 <span>This character sheet uses trademarks and/or copyrights owned by Paizo Publishing, LLC, which are used under Paizo's Community Use Policy. We are expressly prohibited from charging you to use or access this content. This character sheet is not published, endorsed, or specifically approved by Paizo Publishing. For more information about Paizo's Community Use Policy, please visit <a href='http://paizo.com/communityuse'>paizo.com/communityuse</a>. For more information about Paizo Publishing and Paizo products, please visit <a href='http://paizo.com'>paizo.com</a>.</span>        
 </p></div>`;
@@ -13,7 +13,11 @@ register('page', {
     var cls = elementClass('page', null, args, [ 'flex' ]);
 
     var pageNumber = args.numbered ? `<div class='page-number'>${nextPageNumber++}</div>` : '';
-    
+    var copyrightAttribution = paizoCopyrightAttribution;
+
+    if (args.id == 'permission')
+        copyrightAttribution = '';
+
     return `
 <div${id}${cls}>
 ${copyrightAttribution}${pageNumber}${render(args.contents)}
